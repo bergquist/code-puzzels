0740339386dd3857c3d6502b18fc5c165f5513d4
Carl Bergquist <carl.bergquist@gmail.com>
2012-09-18 21:52:58 +0200

    removed tests and move code to one file

--- bestbefore/task1.spec.js
+++ bestbefore/task1.spec.js
@@ -1,48 +1,28 @@
-describe('simple year', function() {
-	describe('should be', function() {
-		it('2001', function() { expect(containsYear([2001])).toBeTruthy(); });
-		it('99', function() { expect(containsYear([99])).toBeTruthy(); });
-		it('32', function() { expect(containsYear([32])).toBeTruthy(); });
-		it('0', function() { expect(containsYear([0])).toEqual(0); });
-	});
-	
-	describe('should not be', function() {
-		it('34, 12', function() { expect(containsYear([34, 12]))});
-	});
-});
-
-
 describe('validate', function() {
-	
 	describe("28/2/12", function() {
 		it("should return 2002-12-28", function() {
-			var result = validate("28/2/12");
-			expect(result).toEqual("2002-12-28");
+			expect(validate("28/2/12")).toEqual("2002-12-28");
 		});
 	});
 	
 	describe("28/28/12", function() {
 		it("should return 2028-12-28", function() {
-			var result = validate("28/28/12");
-			expect(result).toEqual("2028-12-28");
+			expect(validate("28/28/12")).toEqual("2028-12-28");
 		});
 	});
 	
 	describe('2001/02/03', function() {
 		it('should return 2001-02-03', function() {
-			var result = validate('2001/02/03');
-			expect(result).toEqual('2001-02-03');
+			expect(validate('2001/02/03')).toEqual('2001-02-03');
 		})
 	});
 	
 	describe("30/30/30", function() {
 		it("should return 30/30/30 is illegal", function() {
-			var result = validate("30/30/30");
-			expect(result).toEqual("30/30/30 is illegal");
+			expect(validate("30/30/30")).toEqual("30/30/30 is illegal");
 		});		
 	});
 
-	//spotify tests
 	describe('02/4/67', function() {
 		it('should return 2067-02-04', function() {
 			expect(validate('02/4/67')).toEqual('2067-02-04');
@@ -59,6 +39,12 @@ describe('validate', function() {
 		it('should return 2010-11-12', function() {
 			expect(validate('10/11/12')).toEqual('2010-11-12');
 		})
-	});	
+	});
+
+	describe('29/2/2012', function() {
+		it('should return 2012-02-29', function() {
+			expect(validate('29/2/2012')).toEqual('2012-02-29');
+		})
+	});
 });
 
--- bestbefore/validate.js
+++ bestbefore/validate.js
@@ -1,3 +1,73 @@
+var isLeapyear = function(year) {
+	if (year % 4 !== 0) { return false; }
+	if (year % 100 === 0 && year % 400 !== 0) { return false; }
+	return true;
+}
+
+var getDaysPerMonth = function(month, year) {
+	if (month === 2) {
+		if (isLeapyear(year)) {
+			return 29;
+		} else {
+			return 28;
+		}
+	} else {
+		return [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month];
+	}
+}
+
+var deleteFromArray = function(arr, value) {
+	arr.splice(arr.indexOf(value), 1);
+}
+
+var daysAreValidForMonth = function(month, otherNumbers) {
+	var numbers = otherNumbers.slice();
+	numbers.splice(numbers.indexOf(month), 1);
+	numbers.sort(function(a, b) { return a-b; }).reverse();
+	var result = 0;
+	
+	numbers.forEach(function(num) {
+		var potentialYear = numbers.slice();
+		potentialYear.splice(potentialYear.indexOf(num), 1);
+		if (num <= getDaysPerMonth(month, potentialYear) && num > result) {
+			result = num;
+		}
+	});
+
+	return result == 0 ? undefined : result;
+}
+
+var returnValidMonths = function(numbers) {
+	return numbers.filter(function(n) {
+		return n <= 12 && n >= 1;
+	});
+}
+
+var convertInput = function(input) {
+	var arr = input.split('/');
+
+	var numbers = [];
+	arr.forEach(function(n) { numbers.push(parseInt(n)); })
+	numbers.sort();
+	return numbers;
+}
+
+var containsYear = function(numbers) {
+	var result = false;
+
+	numbers.forEach(function(i) {
+		var fourDigit = i.toString().length === 4;
+		var highNumber = i > 31 && i <= 99;
+		var zero = (i === 0);
+		
+		if (fourDigit || highNumber || zero) {
+			result = i;
+		}
+	});
+
+	return result;
+}
+
 var BestBefore = function(input) {
 	this.input = input;
 	this.numbers = convertInput(input);
@@ -45,7 +115,7 @@ BestBefore.prototype.tryToFindYear = function() {
 	if (simpleYear) { 
 		this.finalYear = simpleYear;
 	} else if (this.validMonths.length >= 2) {
-		this.validMonths.sort(function(a, b){ return a - b; });
+		this.validMonths.sort(function(a, b) { return a-b; });
 		this.finalYear = this.validMonths[0];
 		deleteFromArray(this.validMonths, this.finalYear);
 	}
@@ -70,4 +140,10 @@ BestBefore.prototype.validate = function() {
 
 var validate = function(input) {
 	return new BestBefore(input).validate();
+}
+
+if (typeof(exports) !== 'undefined') {
+	exports.validate = validate;
+} else {
+	window.validate = validate;
 }

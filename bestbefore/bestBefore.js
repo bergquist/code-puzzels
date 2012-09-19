f022464d77a02fd1c88ce4aa8fc2f80dc94482b8
Carl Bergquist <carl.bergquist@gmail.com>
2012-09-19 21:58:05 +0200

    moved code around

--- bestbefore/bestBefore.js
+++ bestbefore/bestBefore.js
@@ -1,63 +1,8 @@
 (function() {
-	function isLeapyear(year) {
-		if (year % 4 !== 0) { return false; }
-		if (year % 100 === 0 && year % 400 !== 0) { return false; } //400 is not needed. rephrase if-statement
-		return true;
-	}
-
-	function getDaysPerMonth(month, year) {
-		if (month !== 2) {
-			return [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month];
-		} else {
-			if (isLeapyear(year)) {
-				return 29;
-			} else {
-				return 28;
-			}
-		}
-	}
-
-	function deleteFromArray(arr, value) {
-		arr.splice(arr.indexOf(value), 1);
-	}
-
-	function daysAreValidForMonth(month, otherNumbers) {
-		var numbers = otherNumbers.slice();
-		numbers.splice(numbers.indexOf(month), 1);
-		numbers.sort(function(a, b) { return a-b; }).reverse();
-		var result = 0;
-		
-		numbers.forEach(function(num) {
-			var potentialYear = numbers.slice();
-			potentialYear.splice(potentialYear.indexOf(num), 1);
-			if (num <= getDaysPerMonth(month, potentialYear) && num > result) {
-				result = num;
-			}
-		});
-
-		return result == 0 ? undefined : result;
-	}
-
-	function returnValidMonths(numbers) {
-		return numbers.filter(function(n) {
-			return n <= 12 && n >= 1;
-		});
-	}
-
-	function containsYear(numbers) {
-		var result = false;
-
-		numbers.forEach(function(i) {
-			var fourDigit = i.toString().length === 4;
-			var highNumber = i > 31 && i <= 99;
-			var zero = (i === 0);
-			
-			if (fourDigit || highNumber || zero) {
-				result = i;
-			}
-		});
-
-		return result;
+	if (typeof(exports) !== 'undefined' && typeof(module) !== 'undefined') {
+		module.exports = validate;
+	} else {
+		window.validate = validate;
 	}
 
 	var BestBefore = function(input) {
@@ -65,6 +10,19 @@
 		var numbers = this.numbers = [];
 		input.split('/').forEach(function(n) { numbers.push(parseInt(n)); })
 		this.validMonths = [];
+	}
+
+	BestBefore.prototype.validate = function() {
+		this.validMonths = returnValidMonths(this.numbers);
+		this.tryToFindYear();
+		this.setHighestAsDay();
+		this.setYearIfMissing();
+
+		if (this.finalYear && this.finalMonth && this.finalDay) {
+			return this.formatResponse();
+		} else {
+			return  this.input + ' is illegal';
+		}
 	}
 
 	BestBefore.prototype.setHighestAsDay = function() {
@@ -118,21 +76,67 @@
 		}
 	}
 
-	BestBefore.prototype.validate = function() {
-		this.validMonths = returnValidMonths(this.numbers);
-		this.tryToFindYear();
-		this.setHighestAsDay();
-		this.setYearIfMissing();
+	var validate = function(input) {
+		return new BestBefore(input).validate();
+	}
 
-		if (this.finalYear && this.finalMonth && this.finalDay) {
-			return this.formatResponse();
+	function isLeapyear(year) {
+		if (year % 4 !== 0) { return false; }
+		if (year % 100 === 0 && year % 400 !== 0) { return false; } //400 is not needed. rephrase if-statement
+		return true;
+	}
+
+	function getDaysPerMonth(month, year) {
+		if (month !== 2) {
+			return [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month];
 		} else {
-			return  this.input + ' is illegal';
+			if (isLeapyear(year)) {
+				return 29;
+			} else {
+				return 28;
+			}
 		}
 	}
 
-	var validate = function(input) {
-		return new BestBefore(input).validate();
+	function deleteFromArray(arr, value) {
+		arr.splice(arr.indexOf(value), 1);
+	}
+
+	function daysAreValidForMonth(month, otherNumbers) {
+		var numbers = otherNumbers.slice();
+		numbers.splice(numbers.indexOf(month), 1);
+		numbers.sort(function(a, b) { return a-b; }).reverse();
+		var result = 0;
+		
+		numbers.forEach(function(num) {
+			var potentialYear = numbers.slice();
+			potentialYear.splice(potentialYear.indexOf(num), 1);
+			if (num <= getDaysPerMonth(month, potentialYear) && num > result) {
+				result = num;
+			}
+		});
+
+		return result == 0 ? undefined : result;
+	}
+
+	function returnValidMonths(numbers) {
+		return numbers.filter(function(n) { return n <= 12 && n >= 1; });
+	}
+
+	function containsYear(numbers) {
+		var result = false;
+
+		numbers.forEach(function(i) {
+			var fourDigit = i.toString().length === 4;
+			var highNumber = i > 31 && i <= 99;
+			var zero = (i === 0);
+			
+			if (fourDigit || highNumber || zero) {
+				result = i;
+			}
+		});
+
+		return result;
 	}
 
 	if (typeof(exports) !== 'undefined' && typeof(module) !== 'undefined') {

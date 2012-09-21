32a64cc68469890fe00071f2a6314a97c15e3cbc
Carl Bergquist <carl.bergquist@gmail.com>
2012-09-21 16:01:44 +0200

    adde one more case

--- bestbefore/bestBefore.spec.js
+++ bestbefore/bestBefore.spec.js
@@ -33,24 +33,30 @@ describe('validate', function() {
 	describe('02/4/67', function() {
 		it('should return 2067-02-04', function() {
 			expect(BestBefore.validate('02/4/67')).toEqual('2067-02-04');
-		})
+		});
 	});
 
 	describe('31/9/73', function() {
 		it('should return 2067-02-04 is illegal', function() {
 			expect(BestBefore.validate('31/9/73')).toEqual('31/9/73 is illegal');
-		})
+		});
 	});
 	
 	describe('10/11/12', function() {
 		it('should return 2010-11-12', function() {
 			expect(BestBefore.validate('10/11/12')).toEqual('2010-11-12');
-		})
+		});
 	});
 
 	describe('29/2/2012', function() {
 		it('should return 2012-02-29', function() {
 			expect(BestBefore.validate('29/2/2012')).toEqual('2012-02-29');
+		});
+	});
+
+	describe('10/10/10', function() {
+		it('should return 2010-10-10', function() {
+			expect(BestBefore.validate('10/10/10')).toEqual('2010-10-10');
 		})
 	});
 });
